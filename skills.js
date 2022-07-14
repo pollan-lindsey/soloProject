6204b11b62093275e2e73d2deb74cf3a63abcca1
Lindsey Pollan <lpollan@live.com>
2022-07-14 14:37:19 -0500

    form works

--- skills.js
+++ skills.js
@@ -5,27 +5,36 @@
 
 //get necessary html elements; add .value to the end when referring to them
 
-let password = document.getElementById('password');
-let confirmPW = document.getElementById('confirm-password');
-let firstName = document.getElementById('fname');
+let password = document.querySelector('#password');
+let confirmPW = document.querySelector('#confirm-password');
+let firstName = document.querySelector('.fname');
 let submitBttn = document.getElementById('button');
-let form = document.getElementById('my-form');
+let form = document.querySelector('#my-form');
+
+let fxVid = document.querySelector('#maldonado');
+let createSrc = document.createAttribute('src'); //createSrc.innerText = [add any youtube link; attach source to iframe element]
+
+//let simonebilevideo = link
+
+//add event listener to form 
+let submitForm = (e) => {
+//if passwords don't match, don't submit
+    e.preventDefault();
+// if password === confirm password submit form
+if (password.value === confirmPW.value){
+    form.reset();
+    alert('Thank you for signing up!');
+    console.log(`${password.innerText} ${confirmPW.innerText}`);
+} else {
+    alert('Passwords do not match');
+}}
+
+submitBttn.addEventListener('click', submitForm);
+    // e.preventDefault();
+    // alert('message');
 
 
-
-// //add event listener to form 
-// submitBttn.addEventListener('submit', (event) => {
-// //if passwords don't match, don't submit
-//     event.preventDefault();
-// //if password === confirm password submit form
-// if (password.value === confirmPW.value){
+// submitBttn.addEventListener('click', function () {
 //     form.submit();
 //     alert('Thank you for signing up!')
-// } else {
-//     alert('Passwords do not match');
-// }})
-
-submitBttn.addEventListener('click', function () {
-    form.submit();
-    alert('Thank you for signing up!')
-});
+// });
