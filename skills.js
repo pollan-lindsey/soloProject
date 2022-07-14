5a2b94ed9aae7241c8199f3a094210e53d09dd4f
Lindsey Pollan <lpollan@live.com>
2022-07-13 21:15:20 -0500

    started js

--- skills.js
+++ skills.js
@@ -1,1 +1,15 @@
-//write reset button
+//write submit and reset button
+//call back function
+//event listener
+// add .value to getelementbyid
+
+//get necessary html elements; add .value to the end when referring to them
+
+let password = document.getElementById('password');
+let confirmPW = document.getElementById('confirm-password');
+let firstName = document.getElementById('fname');
+let submitBttn = document.getElementById('button');
+
+//add event listener to submit button 
+//if password === confirm password submit form
+//else alert(passwords do not match)
